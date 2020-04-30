25f647655d2c3381a3885d4e2703826f6cf82660
kevingrimestramore <kevingrimestramore@gmail.com>
2020-05-01 00:56:10 +0100

    Refactor favourite movies selecting; Applied basis for writing reviews for favourite movies

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -1,15 +1,10 @@
 import React from "react";
-import "./movieCard.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../../globals/fontawesome";
 import { Link } from "react-router-dom";
+import "./movieCard.css";
+import "../../globals/fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const MovieCard = ({movie, buttonHandler}) => {
-
-  const handleAddToFavorites = e => {
-    e.preventDefault();
-    buttonHandler(movie.id); 
-  };
+const Movie = ({movie, action}) => {
 
   return (
     <div className="col-sm-3">
@@ -25,7 +20,6 @@ const MovieCard = ({movie, buttonHandler}) => {
           }
         />
         </Link>
-        
         <div className="card-body">
           <h4 className="card-title ">{movie.title}</h4>
           <p>
@@ -38,15 +32,11 @@ const MovieCard = ({movie, buttonHandler}) => {
           </p>
         </div>
         <div className="card-footer">
-          <button type="button" className="btn w-100 btn-primary"
-                onClick={handleAddToFavorites}
-                >
-            Add to Favorites
-          </button>
+           {action(movie)}
         </div>
       </div>
     </div>
   );
 };
 
-export default MovieCard ;
+export default (Movie);
