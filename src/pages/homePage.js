8ab43a12b20166fa1bae938d3032cdf859f475f9
kevingrimestramore <kevingrimestramore@gmail.com>
2020-04-30 14:01:31 +0100

    Refactored templete movie list page, API calls centralised

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,41 +1,16 @@
-import React, {useState, useEffect } from "react";
-import Header from "../components/headerMovieList";
-import MovieList from "../components/movieList";
-import FilterControls from "../components/filterControls";
+import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovies } from "../api/tmdb-api";
 
 const MovieListPage = () => {
-  const [titleFilter, setTitleFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
-
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        return json.results
-      })
-      .then(movies => {
-        setMovies(movies);
-      });
-  })
-  // NEW BLOCK START
-  const genre = Number(genreFilter);
-  let displayedMovies = movies
-    .filter(m => {
-      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
-    })
-    .filter(m => {
-      return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
-    });
 
-  const handleFilterChange = (type, value) => {
-    if (type === "name") setTitleFilter(value);
-    else setGenreFilter(value);
-  };
+  useEffect(() => {
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
+  }, []);
 
   const addToFavorites = movieId => {
     setMovies(movies => {
@@ -48,14 +23,11 @@ const MovieListPage = () => {
   };
 
   return (
-    <>
-      <Header numMovies={displayedMovies.length} />
-      <FilterControls onUserInput={handleFilterChange} />
-      <MovieList
-        movies={displayedMovies}
+      <PageTemplate
+        title='All Movies'
+        movies={movies}
         buttonHandler={addToFavorites}
-      />     {/* CHANGED */}
-    </>
+      />
   );
 };
 
