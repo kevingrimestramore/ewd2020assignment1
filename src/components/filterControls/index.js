a3264cdd30d1e4cfa0c45106bd7ed0dd1ac4457b
kevingrimestramore <kevingrimestramore@gmail.com>
2020-05-01 00:43:47 +0100

    Movie list and genre list now stored in contexts

--- src/components/filterControls/index.js
+++ src/components/filterControls/index.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import "./filterControls.css";
-import { getGenres } from "../../api/tmdb-api";
+import { GenresContext } from '../../contexts/genresContext' 
 
 const FilterControls = props => {
-  const [genres, setGenres] = useState([{ id: '0', name: "All" }]);
-
-  useEffect(() => {
-    getGenres().then(allGenres => {
-      setGenres([genres[0], ...allGenres]);
-    });
-  }, );
+  const context = useContext(GenresContext);
 
   const handleChange = (e, type, value) => {
     e.preventDefault();
@@ -32,18 +26,20 @@ const FilterControls = props => {
             placeholder="Title Search"
             onChange={handleTextChange}
           />
-        <select id="genre" onChange={handleGenreChange}>
-            {genres.map(genre => {
+          <span>Genre:</span>
+          <select id="genre" onChange={handleGenreChange}>
+            {context.genres.map(genre => {
               return (
                 <option key={genre.id} value={genre.id}>
                   {genre.name}
                 </option>
               );
             })}
-        </select>
-       </h4>
+          </select>
+        </h4>
       </div>
     </div>
-);
+  );
 };
+
 export default FilterControls;
--- src/index.js
+++ src/index.js
@@ -7,28 +7,30 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage';
 import FavoriteMoviesPage from './pages/favoriteMoviesPage';
 import MovieReviewPage from './pages/movieReviewPage'
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="jumbotron">
-      <div className="container-fluid">
-        <ul className="navbar-nav text-black">
-          <li className="nav-item">
-          </li>
-        </ul>
+      <div className="jumbotron">
         <SiteHeader />
-        <Switch>
-          <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-          <Route path="/movies/:id" component={MoviePage} />
-          <Route path="/" component={HomePage} />
-          <Route path="/reviews/:id" component={MovieReviewPage} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <div className="container-fluid">
+          <MoviesContextProvider>
+          <GenresContextProvider>
+            <Switch>
+              <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+              <Route path="/movies/:id" component={MoviePage} />
+              <Route path="/" component={HomePage} />
+              <Route path="/reviews/:id" component={MovieReviewPage} />
+              <Redirect from="*" to="/" />
+            </Switch>
+            </GenresContextProvider>
+          </MoviesContextProvider>
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 };
 
