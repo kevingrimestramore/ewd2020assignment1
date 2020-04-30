bcedeeacc11dec803c0c4053777669f5fd15f04b
kevingrimestramore <kevingrimestramore@gmail.com>
2020-05-01 00:20:17 +0100

    Updated site header presentation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import SiteHeader from './components/siteHeader';
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage';
 import FavoriteMoviesPage from './pages/favoriteMoviesPage';
 import MovieReviewPage from './pages/movieReviewPage'
+
 
 const App = () => {
   return (
@@ -14,16 +16,9 @@ const App = () => {
       <div className="container-fluid">
         <ul className="navbar-nav text-black">
           <li className="nav-item">
-            <Link className="nav-link " to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link " to="/movies/favorites">
-              Favorites
-            </Link>
           </li>
         </ul>
+        <SiteHeader />
         <Switch>
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
