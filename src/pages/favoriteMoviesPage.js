34aef27ef1cfcb4bed745973fbefece0aeebab90
kevingrimestramore <kevingrimestramore@gmail.com>
2020-05-02 13:21:31 +0100

    Authentication required for favourites page access

--- src/pages/favoriteMoviesPage.js
+++ src/pages/favoriteMoviesPage.js
@@ -1,17 +1,25 @@
 import React from "react";
 import StubAPI from "../api/stubAPI";
 import PageTemplate from "../components/templateMovieListPage";
-import AddReviewButton from '../components/buttons/addReview'
+import AddReviewButton from '../components/buttons/addReview';
+import { useAuth } from "../contexts/authContext";
+import { Redirect} from "react-router-dom";
 
 const FavoriteMoviesPage = props => {
 
-  return (
-    <PageTemplate
-      movies={StubAPI.getAll()}
-      title={"Favorite Movies"}
-      action={movie => <AddReviewButton movie={movie} />}
-    />
-  );
-};
+  const { authToken } = useAuth();
 
-export default FavoriteMoviesPage;
+  return (
+    <>{authToken ? (
+      <>
+        <PageTemplate
+          movies={StubAPI.getAll()}
+          title={"Favorite Movies"}
+          action={movie => <AddReviewButton movie={movie} />}
+        />
+      </>
+    ) : (
+        <Redirect to="/login" />)}</>
+  );
+    }
+  export default FavoriteMoviesPage;
