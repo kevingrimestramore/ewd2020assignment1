5e8d58ebd35d95a4ccc374d59658d59f47cd951a
kevingrimestramore <kevingrimestramore@gmail.com>
2020-05-02 14:53:38 +0100

    Review form bugfix

--- src/pages/addMovieReviewPage.js
+++ src/pages/addMovieReviewPage.js
@@ -1,13 +1,20 @@
 import React from "react";
 import PageTemplate from "../components/templateMoviePage";
-import ReviewForm from '../components/reviewForm'
+import ReviewForm from '../components/reviewForm';
+import { useAuth } from "../contexts/authContext";
+import { Redirect} from "react-router-dom";
 
 const ReviewFormPage = props => {
 
+  const { authToken } = useAuth();
+
   return (
+    <>{authToken ? (
       <PageTemplate movie={props.location.state.movie}>
           <ReviewForm movie={props.location.state.movie} />
       </PageTemplate>
-  );
+            ) : (
+              <Redirect to="/login" />)}</>
+        );
 };
 export default ReviewFormPage;
